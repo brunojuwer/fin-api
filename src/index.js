98eda0f8c40b4a7a71e5520a1dc06421db3a54c2
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-06-27 13:47:10 -0300

    Created routes to deposit and get statement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const { response } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -7,6 +6,22 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
+
+
+// Middleware
+function verifyExistentAccountCpf(request, response, next){
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer){
+    return response.status(400).json({error: "Customer not found"});
+  }
+
+  request.customer = customer
+
+  return next()
+} 
+
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
@@ -27,5 +42,29 @@ app.post('/account', (request, response) => {
 
   return response.status(201).send();
 })
+
+app.get('/statement', verifyExistentAccountCpf, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer.statement);
+})
+
+app.post('/deposit', verifyExistentAccountCpf, (request, response) => {
+  const { description, amount} = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+  
+  return response.status(201).send();
+})
+
+
 
 app.listen(3333);
